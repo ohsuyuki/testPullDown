8e1cf758a22264f04dbb5a263c66d50678999796
ohsuyuki <[ohsu@liquidinc.asia]>
2018-02-06 13:49:27 +0900

    create

---
+++ testPullDown/SearchTextField.swift
@@ -0,0 +1,21 @@
+//
+//  SearchTextField.swift
+//  testPullDown
+//
+//  Created by osu on 2018/02/05.
+//  Copyright © 2018 osu. All rights reserved.
+//
+
+import UIKit
+
+class SearchTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
